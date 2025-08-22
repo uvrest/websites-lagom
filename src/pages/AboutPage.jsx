7b51be6baade70bce51eadf798f29ac89f9fe098
Uverest <mailtogabrielcoelho@gmail.com>
2025-08-22 08:17:53 -0300

    about hero section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,12 +2,17 @@ import AboutSection from "./sections/AboutSection";
 import TeamSection from "./sections/TeamSection";
 import PageSection from "./sections/_PageSection";
 import teamBackground from "../assets/images/abstract/bg-abstract1.png";
+import AboutHeroSection from "./sections/AboutHeroSection";
 
 const AboutPage = () => {
     return (
         <>
             <PageSection bgColor="paper" container={false} py={0}>
                 <AboutSection />
+            </PageSection>
+
+            <PageSection bgColor="default" container={false} py={0} overlayColor="primary">
+                <AboutHeroSection />
             </PageSection>
 
             <PageSection header={{ title: "Quem somos", align: "center" }} overlayColor="primary" overlayOpacity={0.85} bgImage={teamBackground}>
---
+++ src/pages/sections/AboutHeroSection.jsx
@@ -0,0 +1,46 @@
+import { Box, Typography, alpha } from "@mui/material";
+import AboutHeroImg from "../../assets/images/about/about-hero.jpg";
+import Highlight from "../../components/text/Highlight";
+
+const AboutHeroSection = () => {
+    return (
+        <Box sx={(theme) => ({
+            px: 6,
+            pt: 6,
+            pb: 0
+        })}>
+            <Box
+                sx={(theme) => ({
+                    position: 'relative',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    overflow: 'hidden',
+                    zIndex: 2,
+                    background: theme.palette.secondary.main,
+                    backgroundImage: `url(${AboutHeroImg})`,
+                    backgroundSize: 'cover',
+                    backgroundAttachment: 'fixed',
+                    p: 3,
+                    height: '550px',
+                    borderRadius: '0 100px 0 100px',
+                    '&::before': {
+                        content: "''",
+                        position: 'absolute',
+                        inset: 0,
+                        width: "100%",
+                        height: "100%",
+                        zIndex: -1,
+                        background: alpha(theme.palette.common.black, 0.3),
+                    }
+                })}
+            >
+                <Typography color="white" variant="h5" textAlign='center'>
+                    Equilíbrio é a chave que abre a porta para um futuro <Highlight>+ sustentável</Highlight>
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default AboutHeroSection;
